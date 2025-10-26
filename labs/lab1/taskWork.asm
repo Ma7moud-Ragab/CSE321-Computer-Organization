b4800320e453ca18553b57b944a510f2ee706f67
Mahmoud Ragab <168137264+Ma7moud-Ragab@users.noreply.github.com>
2025-10-26 21:53:51 +0300

    Add data section with personal information output

--- labs/lab1/taskWork.asm
+++ labs/lab1/taskWork.asm
@@ -1,1 +1,20 @@
+.data
+    name: .asciiz "Name: Mahmoud Mohamed Ragab\n"
+    id: .asciiz "ID: 20812022100477\n"
+    course: .asciiz "Course: CSE321-Computer-Organization\n"
 
+.text
+   main: li $v0, 4
+         la $a0, name
+         syscall
+
+         li $v0, 4
+         la $a0, id
+         syscall
+
+         li $v0, 4
+         la $a0, course
+         syscall
+
+         li $v0, 10
+         syscall
